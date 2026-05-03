795a03d1dab8083c2e90369c96ee9f498fd1b915
Sergey Dubovich <losclipp3@gmail.com>
2026-05-04 00:07:11 +0200

    added pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 
 import styles from './page.module.css'
-import module from './test.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={module.title}>Learning Card</div>
+        <div className={styles.title}>Learning Card</div>
         <Image
           className={styles.logo}
           src="/next.svg"
